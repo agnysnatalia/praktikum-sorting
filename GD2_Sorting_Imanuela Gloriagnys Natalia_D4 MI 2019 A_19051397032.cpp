8056f400d06a0b131183753f751896ebf18470f3
agnysnatalia <56225587+agnysnatalia@users.noreply.github.com>
2020-05-01 12:30:11 +0700

    Add files via upload

---
+++ GD2_Sorting_Imanuela Gloriagnys Natalia_D4 MI 2019 A_19051397032.cpp
@@ -0,0 +1,121 @@
+#include <stdio.h>
+#include <iostream>
+#include <stdlib.h>
+
+int data[10], data2[10];
+int n;
+void tukar (int a, int b) {
+	int t;
+	t = data[b];
+	data[b] = data[a];
+	data[a] = t;
+	}
+
+void bubble_sort () 
+	{
+		int i, j;
+	for (int i=1; i<n; i++) 
+		{
+			if (data[j] < data[j-1])
+			tukar (j, j-1);
+		}
+		printf ("Bubble sort selesai!\n");
+		std::cin >> n;
+	}
+	
+
+
+void selection_sort ()
+{
+	int pos, i, j;
+	for (i=0; i<n-1; i++)
+	{
+		pos = i;
+		for (j=i+1; j<n; j++)
+		{
+			if (data[j] < data[pos])
+			pos = j;
+		}
+		if (pos != i) tukar (pos, i);
+	}
+	printf ("Selection sort selesai!\n");
+	std::cin >> n;
+}
+
+void insertion_sort ()
+{
+	int temp, i, j;
+	for (i=1; i<n; i++)
+	{
+		temp = data[i];
+		j = i-1;
+		while (data[j] > temp && j >=0)
+		{
+			data[j+1] = data[j];
+			j--;
+		}
+		data[j+1] = temp;
+	}
+	printf("Insertion sort selesai!\n");
+	std::cin >> n;
+}
+
+void input ()
+{
+	printf("Masukkan jumlah data = ");
+	std::cin>>n;
+	for (int i=0; i<n; i++)
+	{
+		printf("Masukkan data ke-%d = ", (i+1));
+		scanf("%d", &data[i]);
+		data2[i]=data[i];
+	}
+}
+
+void AcakLagi ()
+{
+	for (int i=0; i<n; i++)
+	{
+		data[i] = data2[i];
+	}
+	printf("Data sudah teracak\n");
+	std::cin >> n;
+}
+
+void Tampil ()
+{
+	printf("Data:  ");
+	for (int i=0; i<n; i++)
+	{
+		printf("%d", data[i]);
+	}
+	printf("\n");
+	std::cin >> n;
+}
+
+int  main ()
+{
+	system("cls");
+	int pil;
+	do
+	{
+		system("cls");
+		printf("1. Input Data\n");
+		printf("2. Bubble Sort\n");
+		printf("3. Selesction Sort\n");
+		printf("4. Tampilkan Data\n");
+		printf("5. Acak\n");
+		printf("0. Exit\n");
+		printf("Pilihan = ");
+		std::cin>> pil;
+		switch(pil)
+ 	{
+			case 1: input(); break;
+			case 2: bubble_sort(); break;
+			case 3: selection_sort(); break;
+			case 4: Tampil(); break;
+			case 5: AcakLagi(); break;
+		}
+	}
+	while (pil!=0);
+}
---
+++ UGD_Sorting_Imanuela Gloriagnys Natalia_D4 MI 2019A_19051397032.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#define n 11
+using namespace std;
+int main()
+{
+char a[n] = {'f','z','h','t','u','q'};
+char x,y,z;
+cout<<"\nProgram Sorting Karakter"<<endl;
+cout<<"=========================="<<endl;
+cout<<"\n\nAbjad Sebelum diurutkan : ";
+for (y=0; y<=n-1; y++)
+cout<<a[y];
+cout<<endl;
+
+z=0;
+while(z<=n-2)
+{
+y=0;
+while(y<=n-2-z)
+{
+if(a[y]>a[y+1])
+{
+x=a[y];
+a[y]=a[y+1];
+a[y+1]=x;
+}
+y++;
+}
+z++;
+}
+cout<<"\nAbjad Setelah diurutkan :";
+for(y=0; y<=n-1; y++)
+cout<<a<<a[y];
+
+return 0;
+}
+
